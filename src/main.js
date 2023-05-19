0d6284980375a69c576f5a8b94f70211f0147408
Walefy D. Gonçalves <walefyd1@gmail.com>
2023-05-19 15:27:25 -0300

    Adicionando evento no botão de gerar senha

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { nanoid } from 'nanoid';
 
-const randomPassword = nanoid();
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordElement = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordElement.innerText = randomPassword;
+});
 
 console.log(randomPassword);
