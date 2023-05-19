048dc5f7de6998ec046db1818ce76de56cd634d8
Walefy D. Gonçalves <walefyd1@gmail.com>
2023-05-19 16:08:12 -0300

    Adicionando funcionalidade de cópia

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import { copy } from 'clipboard-copy';
 import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
@@ -7,4 +8,8 @@ const displayPasswordElement = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordElement.innerText = randomPassword;
+});
+
+displayPasswordElement.addEventListener('click', (event) => {
+  copy(event.target.innerText);
 });
